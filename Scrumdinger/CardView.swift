b22cc8b7a60d368f5c4778af847dbd63cb5c6862
Maksim Grebenozhko <m.grebenozhko@tinkoff.ru>
2023-03-31 11:57:56 +0400

    Section 3 Create the card view

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by m.grebenozhko on 31.03.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .padding(.top, 30)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+                    .padding(.leading, 20)
+                    .padding(.top, 20)
+                    .padding(.bottom, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
