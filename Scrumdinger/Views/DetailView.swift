073ae1f33e1f030ad0adc428abb1ef0201915d38
Maksim Grebenozhko <m.grebenozhko@tinkoff.ru>
2023-04-05 10:36:08 +0400

    Section 2. Create the detail view

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by m.grebenozhko on 05.04.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
