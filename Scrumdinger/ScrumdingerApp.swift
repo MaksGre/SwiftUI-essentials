2e639a09668663af0d9860038138ad4c65ea242a
Maksim Grebenozhko <m.grebenozhko@tinkoff.ru>
2023-04-21 07:15:57 +0400

    Section 6. Save data in inactive state

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,17 +10,24 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ScrumsView(scrums: $store.scrums)
-                    .task {
-                        do {
-                            try await store.load()
-                        } catch {
-                            fatalError(error.localizedDescription)
-                        }
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
                     }
+                }
+            }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
             }
         }
     }
